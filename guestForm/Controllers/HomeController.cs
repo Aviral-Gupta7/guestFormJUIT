d8e9f8f45e61b5039485af20c9514e60c5c97a25
Aviral Gupta <ainoxg@gmail.com>
2024-08-25 21:14:04 +0530

    progress on booking form

--- guestForm/Controllers/HomeController.cs
+++ guestForm/Controllers/HomeController.cs
@@ -19,10 +19,22 @@ namespace guestForm.Controllers
             return View();
         }
 
+        public IActionResult Bookingform()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+
+        public IActionResult Bookingform(BookingForm bookingform)
+        {
+            return View();
         }
     }
 }
---
+++ guestForm/Models/BookingForm.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace guestForm.Models
+{
+    public class BookingForm
+    {
+        [Key]
+        public Guid FormId { get; set; }
+
+        // will be added after login is implemented
+
+        //[ForeignKey("UserId")]
+        //public String? UserId { get; set; }
+
+        [Required]
+        public String? FacultyName { get; set; }
+        [Required]
+        public int MobileNo { get; set; }
+        [Required]
+        public String? Department { get; set; }
+        [Required]
+        public int GuestNuMaleSi { get; set; }
+        [Required]
+        public int GuestNuFemaleSi { get; set; }
+        [Required]
+        public int GuestNuCouple { get; set; }
+        [Required]
+        public int GuestNuChildren { get; set; }
+        public DateTime From { get; set; }
+        public DateTime Upto { get; set; }
+        [Required]
+        public String? RelationWithRequisitionerr { get; set; }
+        public bool IsAdminApproved { get; set; }
+        public bool IsRegistrarApproved { get; set; }
+
+    }
+}
